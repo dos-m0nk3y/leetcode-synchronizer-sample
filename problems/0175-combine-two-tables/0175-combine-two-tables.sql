3b744dda95bb5bc27f78ac31a0cbf0884ff76164
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2023-08-15 06:37:17 +0000

    LeetCode Synchronization - Combine Two Tables (MySQL)

--- problems/0175-combine-two-tables/0175-combine-two-tables.sql
+++ problems/0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 
-SELECT person.firstName, person.lastName, address.city, address.state
+SELECT person.firstName AS firstName, person.lastName AS lastName, address.city AS city, address.state AS state
 FROM
   person
-  LEFT OUTER JOIN Address ON person.personId = address.personId;
+  LEFT OUTER JOIN address ON person.personId = address.personId;
