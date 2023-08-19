cc7f6f27edc8d5edd3467b657df1dd2d3fdaadbc
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2023-08-19 09:01:12 +0000

    LeetCode Synchronization - Sales Person (MySQL)

---
+++ problems/0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+SELECT name
+FROM salesperson
+WHERE
+  sales_id NOT IN (
+    SELECT
+      sales_id
+    FROM
+      orders
+      INNER JOIN company USING (com_id)
+    WHERE name = 'RED'
+  );
