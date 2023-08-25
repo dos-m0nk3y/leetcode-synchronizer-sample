1a5430844a404efefcec0da0f20d24480e874faa
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2023-08-25 08:06:43 +0000

    LeetCode Synchronization - Binary Search (C++)

---
+++ problems/0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,29 @@
+static const int fast_io = []()
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
+
+class Solution
+{
+  public:
+    int search(vector<int> &nums, int target)
+    {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] < target)
+                left = mid + 1;
+            else if (nums[mid] > target)
+                right = mid - 1;
+            else
+                return mid;
+        }
+
+        return -1;
+    }
+};
