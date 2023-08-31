4b063277b41574873e7ace297ff5f1ebaf73278b
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2023-08-31 15:51:07 +0000

    LeetCode Synchronization - Repeated Substring Pattern (C++)

--- problems/0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ problems/0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,5 +1,45 @@
+static const int fast_io = []()
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
+
 class Solution
 {
   public:
-    bool repeatedSubstringPattern(string s) { return (s + s).find(s, 1) < s.length(); }
+    bool repeatedSubstringPattern(string s)
+    {
+        string needle = s;
+        s = s.substr(1, -1) + s.substr(0, s.length() - 1);
+
+        vector<int> lps(needle.length(), 0);
+        for (int i = 1; i < needle.length(); i++)
+            do
+            {
+                lps[i] = lps[i] == 0 ? lps[i - 1] : lps[lps[i] - 1];
+                if (needle[i] == needle[lps[i]])
+                {
+                    lps[i] += 1;
+                    break;
+                }
+            } while (lps[i]);
+
+        int left = 0, right = 0;
+        while (left < s.length() && right < needle.length())
+        {
+            if (s[left] == needle[right])
+            {
+                left += 1;
+                right += 1;
+            }
+            else if (right == 0)
+                left += 1;
+            else
+                right = lps[right - 1];
+        }
+
+        return right == needle.length();
+    }
 };
