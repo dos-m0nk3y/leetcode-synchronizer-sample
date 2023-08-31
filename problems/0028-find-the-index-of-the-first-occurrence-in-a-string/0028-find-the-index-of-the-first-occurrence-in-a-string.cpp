50ed3ef102a30c441a5b9cd4f490832190a22dd2
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2023-08-31 15:32:56 +0000

    LeetCode Synchronization - Find the Index of the First Occurrence in a String (C++)

--- problems/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ problems/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,5 +1,42 @@
+static const int fast_io = []()
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
+
 class Solution
 {
   public:
-    int strStr(string haystack, string needle) { return haystack.find(needle); }
+    int strStr(string haystack, string needle)
+    {
+        vector<int> lps(needle.length(), 0);
+        for (int i = 1; i < needle.length(); i++)
+            do
+            {
+                lps[i] = lps[i] == 0 ? lps[i - 1] : lps[lps[i] - 1];
+                if (needle[i] == needle[lps[i]])
+                {
+                    lps[i] += 1;
+                    break;
+                }
+            } while (lps[i]);
+
+        int left = 0, right = 0;
+        while (left < haystack.length() && right < needle.length())
+        {
+            if (haystack[left] == needle[right])
+            {
+                left += 1;
+                right += 1;
+            }
+            else if (right == 0)
+                left += 1;
+            else
+                right = lps[right - 1];
+        }
+
+        return right == needle.length() ? left - needle.length() : -1;
+    }
 };
